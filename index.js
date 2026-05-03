db1ba562ca5e86b20e34ed0b460cddd42b2985b5
Shreyashppp <kalmenghroshani@gmail.com>
2026-05-03 23:22:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.once('ready', () => {
   console.log("Bot Online");
 });
 
-// 👇 ADD THIS LINE HERE
-console.log("MTUwMDUzOTc0MzE2MTgxMTAxNQ.GRZeM8.OLNERe7jTcpZoPYl-4_1RlZNf-jUUQpJHt4Tag:", process.env.TOKEN);
+// ✅ correct debug
+console.log("TOKEN:", process.env.TOKEN);
 
 client.login(process.env.TOKEN);
