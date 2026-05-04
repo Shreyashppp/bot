a7045bb93d51d5ce6fd67145aaceac432a6ede2f
Shreyashppp <kalmenghroshani@gmail.com>
2026-05-04 10:41:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,34 @@
-const { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-const commands = [
-  new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
+client.commands = new Collection();
 
-  new SlashCommandBuilder()
-    .setName('kick')
-    .setDescription('Kick a user')
-    .addUserOption(option =>
-      option.setName('user').setDescription('User to kick').setRequired(true)
-    ),
+// Load commands
+const fs = require('fs');
+const files = fs.readdirSync('./commands');
 
-  new SlashCommandBuilder()
-    .setName('ban')
-    .setDescription('Ban a user')
-    .addUserOption(option =>
-      option.setName('user').setDescription('User to ban').setRequired(true)
-    )
-].map(cmd => cmd.toJSON());
+for (const file of files) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-
-(async () => {
-  try {
-    console.log('Registering commands...');
-    await rest.put(
-      Routes.applicationCommands("1500539743161811015"),
-      { body: commands }
-    );
-    console.log('Commands registered!');
-  } catch (error) {
-    console.error(error);
-  }
-})();
+client.once('ready', () => {
+  console.log("Bot Online");
+});
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('🏓 Pong!');
-  }
+  const cmd = client.commands.get(interaction.commandName);
 
-  if (interaction.commandName === 'kick') {
-    const user = interaction.options.getUser('user');
-    const member = interaction.guild.members.cache.get(user.id);
+  if (!cmd) return;
 
-    if (!member) return interaction.reply("User not found");
-
-    await member.kick();
-    await interaction.reply(`${user.tag} kicked 👢`);
-  }
-
-  if (interaction.commandName === 'ban') {
-    const user = interaction.options.getUser('user');
-    const member = interaction.guild.members.cache.get(user.id);
-
-    if (!member) return interaction.reply("User not found");
-
-    await member.ban();
-    await interaction.reply(`${user.tag} banned 🔨`);
-  }
-});
-
-client.on('clientReady', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+  await cmd.execute({
+    reply: (msg) => interaction.reply(msg)
+  });
 });
 
 client.login(process.env.TOKEN);
