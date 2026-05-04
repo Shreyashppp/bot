bc428f5813e9038a110491dc19cd376f9cf51de1
Shreyashppp <kalmenghroshani@gmail.com>
2026-05-04 11:21:33 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,43 +1,36 @@
 require('dotenv').config();
 const { REST, Routes } = require('discord.js');
-const fs = require('fs');
 
-const commands = [];
-const files = fs.readdirSync('./commands');
-
-for (const file of files) {
-  const command = require(`./commands/${file}`);
-  commands.push({
-    name: command.name,
-    description: command.name + " command"
-  });
-}
+const commands = [
+  {
+    name: "ping",
+    description: "Ping command"
+  },
+  {
+    name: "help",
+    description: "Help command"
+  },
+  {
+    name: "ban",
+    description: "Ban a user",
+    options: [
+      {
+        name: "user",
+        type: 6,
+        description: "User to ban",
+        required: true
+      }
+    ]
+  }
+];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
-  try {
-    console.log("Registering commands...");
+  await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: commands }
+  );
 
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: commands }
-    );
-
-    console.log("Commands registered!");
-  } catch (error) {
-    console.error(error);
-  }
+  console.log("Commands registered");
 })();
-{
-  name: "ban",
-  description: "Ban a user",
-  options: [
-    {
-      name: "user",
-      type: 6,
-      description: "User to ban",
-      required: true
-    }
-  ]
-}
