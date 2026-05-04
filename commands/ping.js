02aef798bcc7bf1c2d6f8aebdb3af9f74e96dad8
Shreyashppp <kalmenghroshani@gmail.com>
2026-05-04 14:23:29 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,12 +2,13 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "ping",
+  category: "utility",
+
   async execute(interaction) {
     const embed = new EmbedBuilder()
       .setTitle("🏓 Pong!")
-      .setDescription("Bot is working perfectly")
-      .setColor("Green")
-      .setFooter({ text: "Your Bot" });
+      .setDescription("Bot working perfectly")
+      .setColor("Green");
 
     await interaction.reply({ embeds: [embed] });
   }
