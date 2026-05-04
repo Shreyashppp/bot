7b35f61e29ae21eeb6cf9499f16b3cb460148c47
Shreyashppp <kalmenghroshani@gmail.com>
2026-05-04 14:24:22 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,35 +1,19 @@
-const { EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const { PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
   name: "ban",
+  category: "moderation",
 
   async execute(interaction) {
+    if (!interaction.member.permissions.has(PermissionFlagsBits.BanMembers))
+      return interaction.reply({ content: "No permission", ephemeral: true });
 
-    // Check permission
-    if (!interaction.member.permissions.has(PermissionFlagsBits.BanMembers)) {
-      return interaction.reply({
-        content: "❌ You don't have permission to use this command",
-        ephemeral: true
-      });
-    }
-
-    // Get user
     const user = interaction.options.getUser('user');
     const member = interaction.guild.members.cache.get(user.id);
 
-    if (!member) {
-      return interaction.reply("❌ User not found");
-    }
+    if (!member) return interaction.reply("User not found");
 
-    // Ban user
     await member.ban();
-
-    // Embed response
-    const embed = new EmbedBuilder()
-      .setTitle("🔨 User Banned")
-      .setDescription(`${user.tag} has been banned`)
-      .setColor("Red");
-
-    await interaction.reply({ embeds: [embed] });
+    await interaction.reply(`Banned ${user.tag}`);
   }
 };
